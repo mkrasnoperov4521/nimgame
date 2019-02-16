a7c7985bf85d57591cb0bea3081271e344a8c237
Maxim Krasnoperov <mkrasnoperov18@students.polytechnic.org>
2019-02-15 19:28:00 -0600

    Initial Commit

---
+++ project3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project3
+//
+//  Created by Maxim Krasnoperov on 2/15/19.
+//  Copyright © 2019 Maxim Krasnoperov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
